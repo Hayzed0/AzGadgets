983539ade7f7e6e525a32f4260f7e893b570de04
Kehinde Azeez <ak22aed@herts.ac.uk>
2025-04-20 17:16:01 +0100

    some bug

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, "https://az-gadgets.vercel.app"],
+    origin: [process.env.CLIENT_URL, "https://az-gadgets.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
