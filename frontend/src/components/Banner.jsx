59b5dd19cf6d4c9fdbbe4533853f7a3b7cd7eb94
Kehinde Azeez <ak22aed@herts.ac.uk>
2025-04-29 19:51:43 +0100

    some images

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -3,40 +3,54 @@ import clearance from "../assets/clearance-deal.avif";
 import samsung from "../assets/samsung-banner.avif";
 import ipadAir from "../assets/banner-ipad.avif";
 import nintendo from "../assets/banner-nitendo.avif";
+import { Helmet } from "react-helmet";
 
 const Banner = () => {
   return (
-    <section className="flex flex-col mt-4 p-2">
-      <div className="flex flex-col items-center gap-6 justify-between md:flex-row">
-        <div className="flex w-full h-60 md:h-90 lg:h-auto">
-          <img
-            src={clearance}
-            alt="Clearance sales banner"
-            className="h-auto w-full rounded-lg"
-            loading="eager"
-          />
+    <>
+      <Helmet>
+        <link
+          rel="preload"
+          as="image"
+          href={clearance}
+          type="image/avif"
+          fetchpriority="high"
+        />
+      </Helmet>
+
+      <section className="flex flex-col mt-4 p-2">
+        <div className="flex flex-col items-center gap-6 justify-between md:flex-row">
+          <div className="flex w-full h-60 md:h-90 lg:h-auto">
+            <img
+              src={clearance}
+              alt="Clearance sales banner"
+              className="h-auto w-full rounded-lg"
+              loading="eager"
+              fetchpriority="high"
+            />
+          </div>
+          <div className="flex w-full h-60 md:h-90 lg:h-auto">
+            <img
+              src={samsung}
+              alt="Samsung Deal Banner"
+              className="h-auto w-full rounded-lg"
+              loading="eager"
+            />
+          </div>
+          <div className="flex w-full h-60 md:h-90 lg:h-auto">
+            <img
+              src={ipadAir}
+              alt="IpadAir banner"
+              className="h-auto w-full rounded-lg"
+              loading="eager"
+            />
+          </div>
         </div>
-        <div className="flex w-full h-60 md:h-90 lg:h-auto">
-          <img
-            src={samsung}
-            alt="Samsung Deal Banner"
-            className="h-auto w-full rounded-lg"
-            loading="eager"
-          />
+        <div className="mt-8">
+          <img src={nintendo} alt="nintendo" loading="eager" fetchpriority="high"/>
         </div>
-        <div className="flex w-full h-60 md:h-90 lg:h-auto">
-          <img
-            src={ipadAir}
-            alt="IpadAir banner"
-            className="h-auto w-full rounded-lg"
-            loading="eager"
-          />
-        </div>
-      </div>
-      <div className="mt-8">
-        <img src={nintendo} alt="nintendo" loading="eager" />
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
